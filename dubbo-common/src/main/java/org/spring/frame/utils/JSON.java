56648f7514025e581021f2fce17090585421aeec
HeYixuan <15517551511@126.com>
2016-07-19 15:50:24 +0800

    提交

---
+++ dubbo-common/src/main/java/org/spring/frame/utils/JSON.java
@@ -0,0 +1,42 @@
+package org.spring.frame.utils;
+
+/**
+ * JSON返回工具类
+ * @author 何壹轩
+ * @version 2016-06-28
+ */
+public class JSON implements java.io.Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private boolean success = false;
+
+	private String msg = "";
+
+	private Object obj = null;
+
+	public boolean isSuccess() {
+		return success;
+	}
+
+	public void setSuccess(boolean success) {
+		this.success = success;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
+	public Object getObj() {
+		return obj;
+	}
+
+	public void setObj(Object obj) {
+		this.obj = obj;
+	}
+
+}
---
+++ dubbo-common/src/main/java/org/spring/frame/utils/MD5_String.java
@@ -0,0 +1,37 @@
+package org.spring.frame.utils;
+
+import java.util.Random;
+
+/**
+ * 生成随机字符串工具类
+ * @author 何壹轩
+ * @version 2016-06-28
+ */
+public class MD5_String {
+	/**
+	   * 产生随机字符串
+	   * */
+	private static Random randGen = null;
+	private static char[] numbersAndLetters = null;
+
+	public static final String randomString(int length) {
+	         if (length < 1) {
+	             return null;
+	         }
+	         if (randGen == null) {
+	                randGen = new Random();
+	                numbersAndLetters = ("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz").toCharArray();
+	                 }
+	         char [] randBuffer = new char[length];
+	         for (int i=0; i<randBuffer.length; i++) {
+	             randBuffer[i] = numbersAndLetters[randGen.nextInt(60)];
+	         }
+	         return new String(randBuffer);
+	}
+	
+	public static void main(String[] args) {
+		String s=randomString(60);
+		System.out.println(s);
+		
+	}
+}
