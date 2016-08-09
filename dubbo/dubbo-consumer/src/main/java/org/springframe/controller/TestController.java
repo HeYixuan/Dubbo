9ff7d61a9bde6371a3f48466da92c3ee50cddfbd
HeYixuan <15517551511@126.com>
2016-08-09 08:20:21 +0800

    提交

---
+++ dubbo/dubbo-consumer/src/main/java/org/springframe/controller/TestController.java
@@ -0,0 +1,27 @@
+package org.springframe.controller;
+
+import org.springframe.service.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/test")
+public class TestController {
+	
+	@Autowired(required=true)
+	private TestService testService;
+	
+	@RequestMapping("/testDubbo")
+	@ResponseBody
+	public String sayDubbo(){
+		return testService.testSayDubbo();
+	}
+
+	@RequestMapping("/testSay")
+	@ResponseBody
+	public String sayHello(String name){
+		return testService.say(name);
+	}
+}
